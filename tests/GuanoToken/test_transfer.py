13a76254aea5e7c2ee275003a7c5266cdaf3c98a
Pigeon <86813921+etherpigeon@users.noreply.github.com>
2021-07-02 10:39:13 -0400

    test: guano transfer function

---
+++ tests/GuanoToken/test_transfer.py
@@ -0,0 +1,31 @@
+import brownie
+import pytest
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup(alice, guano):
+    guano.mint(alice, 10 ** 18, {"from": alice})
+
+
+def test_modify_sender_and_recipient_balances(alice, bob, chain, guano):
+    guano.transfer(bob, 10 ** 18, {"from": alice})
+    assert guano.balanceOf(bob, block_identifier=chain.height - 1) == 0
+    assert guano.balanceOf(alice) == 0
+    assert guano.balanceOf(bob) == 10 ** 18
+
+
+def test_log_transfer_event(alice, bob, guano):
+    tx = guano.transfer(bob, 10 ** 18, {"from": alice})
+    expected = dict(_from=alice, _to=bob, _value=10 ** 18)
+    assert "Transfer" in tx.events
+    assert tx.events["Transfer"] == expected
+
+
+def test_return_value(alice, bob, guano):
+    tx = guano.transfer(bob, 10 ** 18, {"from": alice})
+    assert tx.return_value is True
+
+
+def test_sender_insufficient_balance(alice, bob, guano):
+    with brownie.reverts("Integer underflow"):
+        guano.transfer(bob, 10 ** 21, {"from": alice})
