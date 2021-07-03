e07a8078e038eefbfd12717d8039ea6b0ca91f80
Pigeon <86813921+etherpigeon@users.noreply.github.com>
2021-07-03 14:11:38 -0400

    test: add am3crv_nest fixture

--- tests/fixtures/deployments.py
+++ tests/fixtures/deployments.py
@@ -4,3 +4,8 @@ import pytest
 @pytest.fixture(scope="module")
 def guano(alice, GuanoToken):
     return GuanoToken.deploy({"from": alice})
+
+
+@pytest.fixture(scope="module")
+def am3crv_nest(alice, NestAM3CRV):
+    return NestAM3CRV.deploy({"from": alice})
