a25890e3b51f75f08b3df3f57bcf6127e6ed6985
Pigeon <86813921+etherpigeon@users.noreply.github.com>
2021-07-22 11:08:19 -0400

    fix: am3pool fixture
    Stabilize the pool's balances correctly

--- tests/fixtures/deployments.py
+++ tests/fixtures/deployments.py
@@ -11,16 +11,20 @@ def am3crv_nest(alice, NestAM3CRV):
     return NestAM3CRV.deploy({"from": alice})
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def am3pool(interface, coins, decimals):
     pool = interface.StableSwap("0x445FE580eF8d70FF569aB36e80c647af338db351")
     pool.donate_admin_fees({"from": pool.owner()})
 
     scalers = [18 - precision for precision in decimals]
     balances = [pool.balances(i) * 10 ** precision for i, precision in enumerate(scalers)]
-    optimal_balances = [max(balances) // 10 ** precision for precision in scalers]
+    mint_amounts = [
+        (max(balances) - balances[i]) // 10 ** precision for i, precision in enumerate(scalers)
+    ]
 
-    for coin, optimal_balance in zip(coins, optimal_balances):
-        coin._mint_for_testing(pool, optimal_balance)
+    for coin, mint_amt in zip(coins, mint_amounts):
+        if mint_amt == 0:
+            continue
+        coin._mint_for_testing(pool, mint_amt)
 
     return pool
